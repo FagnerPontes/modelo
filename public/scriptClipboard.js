c6bbdaa9ec675aee001be7d2fa80fe452bd83a48
fagnergithub <pontes.fagner@gmail.com>
2024-09-15 22:24:22 -0300

    update

---
+++ public/scriptClipboard.js
@@ -0,0 +1,67 @@
+class Clipboard {
+ constructor(texto) {
+  this.texto = texto;
+  this.modalElement = null;
+  this.createModal(texto);
+ }
+
+ createModal(texto) {
+  // Cria o container do modal
+  navigator.clipboard.writeText(texto).then(() => {
+   const modal = document.createElement('div');
+
+   // Adiciona estilos diretamente via JavaScript
+   modal.style.setProperty('display', 'flex');
+   modal.style.setProperty('position', 'fixed');
+   modal.style.setProperty('left', 'calc(50vw - 9rem)');
+   modal.style.setProperty('top', '0');
+   modal.style.setProperty('width', '18rem');
+   modal.style.setProperty('height', '5rem');
+   modal.style.setProperty('background-color', 'var(--backColor3)');
+   modal.style.setProperty('justify-content', 'center');
+   modal.style.setProperty('align-items', 'center');
+   modal.style.setProperty('border-radius', '2.5rem');
+   modal.style.setProperty('z-index', '1000');
+   modal.style.setProperty('padding', '.5rem');
+
+   // Adiciona o texto passado na criação do modal
+   const modalText = document.createElement('p');
+   modalText.style.setProperty('color', 'var(--fontColor1)');
+   modalText.style.setProperty('font-size', '1.5rem');
+   modalText.style.setProperty('text-align', 'center');
+   modalText.style.setProperty('justify-content', 'center');
+   modalText.style.setProperty('flex', '1');
+   modalText.style.setProperty('padding', '.5rem');
+
+   modalText.textContent = texto;
+
+   // Adiciona o texto e o botão ao conteúdo do modal
+   modal.appendChild(modalText);
+   document.body.appendChild(modal);
+
+   // Armazena o modal para referência futura
+   this.modalElement = modal;
+
+   var opacity = 1.0;
+   // Definindo o intervalo de animação
+   const intervalId = setInterval(() => {
+    opacity -= 0.025;
+    this.modalElement.style.setProperty('opacity', `${opacity}`);
+   }, 50);
+
+   // Interrompendo o intervalo após 5 segundos
+   setTimeout(() => {
+    clearInterval(intervalId);
+    this.closeModal();
+   }, 2000);
+  });
+ }
+
+ // Método para fechar o modal
+ closeModal() {
+  if (this.modalElement) {
+   this.modalElement.style.setProperty('display', 'none');
+   this.modalElement.remove();
+  }
+ }
+}
--- public/scriptModal.js
+++ public/scriptModal.js
@@ -7,71 +7,61 @@ class Modal {
 
  createModal(texto) {
   // Cria o container do modal
-  navigator.clipboard.writeText(texto).then(() => {
-   const modal = document.createElement('div');
+  const modal = document.createElement('div');
 
-   // Adiciona estilos diretamente via JavaScript
-   modal.style.setProperty('display', 'none');
-   modal.style.setProperty('position', 'fixed');
-   modal.style.setProperty('left', 'calc(50vw - 9rem)');
-   modal.style.setProperty('top', 'calc(50vh - 6rem)');
-   modal.style.setProperty('width', '18rem');
-   modal.style.setProperty('height', '9rem');
-   modal.style.setProperty('background-color', 'var(--fontColor4)');
-   modal.style.setProperty('justify-content', 'center');
-   modal.style.setProperty('align-items', 'center');
-   modal.style.setProperty('border-radius', '10px');
-   modal.style.setProperty('z-index', '1000');
-   modal.style.setProperty('padding', '.5rem');
+  // Adiciona estilos diretamente via JavaScript
+  modal.style.setProperty('display', 'flex');
+  modal.style.setProperty('flex-direction', 'column');
+  modal.style.setProperty('position', 'fixed');
+  modal.style.setProperty('left', 'calc(50vw - 9rem)');
+  modal.style.setProperty('top', 'calc(50vh - 6rem)');
+  modal.style.setProperty('width', '18rem');
+  modal.style.setProperty('min-height', '6rem');
+  modal.style.setProperty('justify-content', 'center');
+  modal.style.setProperty('align-items', 'center');
+  modal.style.setProperty('z-index', '1000');
+  modal.style.setProperty('padding', '1rem');
+  modal.style.setProperty('background-color', 'var(--backColor1s)');
+  modal.style.setProperty('border-radius', '10px');
+  modal.style.setProperty('border', '2px solid var(--fontColor3)');
 
-   // Cria o conteúdo do modal
-   const modalContent = document.createElement('div');
 
-   // Estilos para o conteúdo do modal
-   modalContent.style.setProperty('display', 'flex');
-   modalContent.style.setProperty('flex-direction', 'column');
-   modalContent.style.setProperty('background-color', 'var(--backColor1s)');
-   modalContent.style.setProperty('border-radius', '6px');
-   modalContent.style.setProperty('justify-content', 'center');
-   modalContent.style.setProperty('align-items', 'center');
-   modalContent.style.setProperty('flex', '1');
-   modalContent.style.setProperty('padding', '.5rem');
-   modalContent.style.setProperty('height', '8rem');
 
-   // Adiciona o texto passado na criação do modal
-   const modalText = document.createElement('p');
-   modalText.style.setProperty('color', 'var(--fontColor1)');
-   modalText.style.setProperty('text-align', 'center');
-   modalText.style.setProperty('justify-content', 'center');
-   modalText.style.setProperty('flex', '1');
-   modalText.style.setProperty('padding', '.5rem');
+  // Adiciona o texto passado na criação do modal
+  const modalText = document.createElement('p');
+  modalText.style.setProperty('color', 'var(--fontColor3)');
+  modalText.style.setProperty('font-size', '1.25rem');
+  modalText.style.setProperty('text-align', 'center');
+  modalText.style.setProperty('justify-content', 'center');
+  modalText.style.setProperty('flex', '1');
+  modalText.style.setProperty('padding-bottom', '1rem');
 
-   modalText.textContent = texto;
+  modalText.textContent = texto;
 
-   // Botão de fechar
-   const closeButton = document.createElement('button');
-   closeButton.style.setProperty('display', 'flex');
-   closeButton.style.setProperty('justify-content', 'center');
-   closeButton.style.setProperty('width', '4rem');
-   closeButton.style.setProperty('height', '2rem');
-   closeButton.style.setProperty('padding', '.5rem');
-   closeButton.style.setProperty('border-radius', '8px');
+  // Botão de fechar
+  const closeButton = document.createElement('button');
+  closeButton.style.setProperty('color', 'var(--fontColor3)');
+  closeButton.style.setProperty('display', 'flex');
+  closeButton.style.setProperty('font-size', '1rem');
+  closeButton.style.setProperty('justify-content', 'center');
+  closeButton.style.setProperty('align-items', 'center');
+  closeButton.style.setProperty('width', '3rem');
+  closeButton.style.setProperty('height', '3rem');
+  closeButton.style.setProperty('border-radius', '1.5rem');
 
-   closeButton.textContent = 'OK';
-   closeButton.addEventListener('click', () => this.closeModal());
 
-   // Adiciona o texto e o botão ao conteúdo do modal
-   modalContent.appendChild(modalText);
-   modalContent.appendChild(closeButton);
-   modal.appendChild(modalContent);
+  closeButton.textContent = 'OK';
+  closeButton.addEventListener('click', () => this.closeModal());
 
-   // Adiciona o modal ao body
-   document.body.appendChild(modal);
+  // Adiciona o texto e o botão ao conteúdo do modal
+  modal.appendChild(modalText);
+  modal.appendChild(closeButton);
 
-   // Armazena o modal para referência futura
-   this.modalElement = modal;
-   this.modalElement.style.setProperty('display', 'flex');
-  });
+  // Adiciona o modal ao body
+  document.body.appendChild(modal);
+  // Armazena o modal para referência futura
+
+  this.modalElement = modal;
  }
 
  // Método para fechar o modal
